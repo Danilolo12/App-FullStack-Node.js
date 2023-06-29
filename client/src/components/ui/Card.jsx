271d51f9987225597d985494e1f607eade8f4fcc
Daniel Ramos <danieleduardoramos977@gmail.com>
2023-06-29 15:19:29 -0500

    App FullStack

---
+++ client/src/components/ui/Card.jsx
@@ -0,0 +1,3 @@
+export function Card({ children }) {
+  return <div className="bg-slate-700 max-w-md w-full p-10 rounded-md">{children}</div>;
+}
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+function HomePage() {
+  return (
+  <section className="bg-teal-800 flex justify-center items-center">
+    <header className="bg-zinc-800 p-10">
+      <h1 className="text-5xl py-2 font-bold">React Tasks</h1>
+      <p className="text-md text-slate-400">
+       HI!
+       Here you can create and update and delete tasks, this app works like a calendar...
+       Enjoy.
+      </p>
+
+      <Link
+        className="bg-emerald-700 text-white px-4 py-2 rounded-md mt-4 inline-block"
+        to="/register"
+      >
+        Get Started
+      </Link>
+    </header>
+  </section>
+  );
+}
+
+export default HomePage;
